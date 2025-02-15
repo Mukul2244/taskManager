72282e80d52c1ece616224179d432617a5fcdebd
parasohri <150348022+parasohri@users.noreply.github.com>
2025-02-15 22:55:29 +0530

    Update MongoDB connection to use environment variable for URL

--- backend/db/mongodb.js
+++ backend/db/mongodb.js
@@ -1,6 +1,6 @@
 import fastify from "fastify";
 import mongoose from "mongoose";
-export const dbconnect=()=>{ mongoose.connect("mongodb+srv://ohriparas2005:8O52nrdNPJu5qqto@cluster0.gpenh.mongodb.net/paras",).then(() => {
+export const dbconnect=()=>{ mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.log("Error connecting to MongoDB", err);
